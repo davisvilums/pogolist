1979dab91d62cba5e4236a12d64be3bd3a729a3c
Davis Vilums <davisvilums@gmail.com>
2021-08-01 20:20:11 +0100

    Cleared sorting fields

--- src/components/SortingFields.js
+++ src/components/SortingFields.js
@@ -14,7 +14,33 @@ export default function SortingFields({ data, onSorting }) {
   };
   return (
     <div className='PokemonSort'>
-      {data[0] &&
+
+<strong onClick={() => onSortingChange("cp")} key={"cp"}>
+        {"CP"}
+        {sortingField &&
+          sortingField === "cp" &&
+          (sortingOrder === 'asc' ? '>' : '<')}
+      </strong>
+      <strong onClick={() => onSortingChange("name")} key={"name"}>
+        {"Name"}
+        {sortingField &&
+          sortingField === "name" &&
+          (sortingOrder === 'asc' ? '>' : '<')}
+      </strong>
+      <strong onClick={() => onSortingChange("order")} key={"order"}>
+        {"Order"}
+        {sortingField &&
+          sortingField === "order" &&
+          (sortingOrder === 'asc' ? '>' : '<')}
+      </strong>
+      <strong onClick={() => onSortingChange("id")} key={"id"}>
+        {"ID"}
+        {sortingField &&
+          sortingField === "id" &&
+          (sortingOrder === 'asc' ? '>' : '<')}
+      </strong>
+      
+      {/* {data[0] &&
         columns.map((heading) => (
           <strong onClick={() => onSortingChange(heading)} key={heading}>
             {heading}
@@ -22,7 +48,7 @@ export default function SortingFields({ data, onSorting }) {
               sortingField === heading &&
               (sortingOrder === 'asc' ? '>' : '<')}
           </strong>
-        ))}
+        ))} */}
     </div>
   );
 }
