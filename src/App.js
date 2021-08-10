027ec66a7074a9ee4d6f905f42e5c9b6f81b7cac
Davis Vilums <davis@reci.pe>
2021-08-10 16:59:55 +0100

    test changes

--- src/App.js
+++ src/App.js
@@ -83,7 +83,7 @@ function App() {
             onClick={handleDrawerOpen}
             className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
           >
-            <MenuIcon />
+            <MenuIcon />213
           </IconButton> */}
           <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
             Personal Pokedex
